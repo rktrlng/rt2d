e01bb5c1db7a949ed06edce6fc1605256f42e855
rktrlng <rik.teerling@gmail.com>
2015-10-04 15:20:52 +0200

    don't attempt to build on win32/msys (generate VS project)

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,13 @@ fi
 mkdir -p build
 cd build
 cmake -Wno-dev ..
-make
 
-./demo
+if [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "msys" ]; then
+	echo "Double-click the 'rt2d.sln' file in the 'build' directory to open the project in Visual Studio."
+	echo "Right-click 'demo' tab and select 'Set as StartUp Project'. Then press <ctrl>-F5."
+else
+	make
+	./demo
+fi
 
 cd ..
