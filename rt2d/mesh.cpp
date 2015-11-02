6ab58ec116070f3d2923ce879ec12e23f6991ede
rktrlng <rik.teerling@gmail.com>
2015-11-02 20:48:15 +0100

    spritemesh by vector of vertices

--- rt2d/mesh.cpp
+++ rt2d/mesh.cpp
@@ -35,52 +35,39 @@ void Mesh::generateSpriteMesh(int width, int height, float pivotx, float pivoty,
 	
 	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A sprite has 1 face (quad) with 2 triangles each, so this makes 1*2=2 triangles, and 2*3 vertices
-	const GLfloat g_vertex_buffer_data[] = {
-		-width * pivotx, -height * pivoty, 0.0f,
-		-width * pivotx, height - (height * pivoty), 0.0f,
-		 width - (width * pivotx), height - (height * pivoty), 0.0f,
-		 
-		 width - (width * pivotx), height - (height * pivoty), 0.0f,
-		 width - (width * pivotx), -height * pivoty, 0.0f,
-		-width * pivotx, -height * pivoty, 0.0f
-	};
-/*
-	// Normals for each vertex.
-	const GLfloat g_normal_buffer_data[] = {
-		0.0f, 0.0f, -1.0f,
-		0.0f, 0.0f, -1.0f,
-		0.0f, 0.0f, -1.0f,
-		
-		0.0f, 0.0f, -1.0f,
-		0.0f, 0.0f, -1.0f,
-		0.0f, 0.0f, -1.0f
-	};
-*/
-	// UV coordinates for each vertex.
-	const GLfloat g_uv_buffer_data[] = {
-		0.0f, uvheight,
-		0.0f, 0.0f,
-		uvwidth, 0.0f,
-		
-		uvwidth, 0.0f,
-		uvwidth, uvheight,
-		0.0f, uvheight
-	};
+	std::vector<glm::vec3> vertices;
+	
+	vertices.push_back(glm::vec3(-width * pivotx, -height * pivoty, 0.0f));
+	vertices.push_back(glm::vec3(-width * pivotx, height - (height * pivoty), 0.0f));
+	vertices.push_back(glm::vec3(width - (width * pivotx), height - (height * pivoty), 0.0f));
+	vertices.push_back(glm::vec3(width - (width * pivotx), height - (height * pivoty), 0.0f));
+	vertices.push_back(glm::vec3(width - (width * pivotx), -height * pivoty, 0.0f));
+	vertices.push_back(glm::vec3(-width * pivotx, -height * pivoty, 0.0f));
 
+	// UV coordinates for each vertex.
+	std::vector<glm::vec2> uvs;
+	
+	uvs.push_back(glm::vec2(0.0f, uvheight));
+	uvs.push_back(glm::vec2(0.0f, 0.0f));
+	uvs.push_back(glm::vec2(uvwidth, 0.0f));
+	uvs.push_back(glm::vec2(uvwidth, 0.0f));
+	uvs.push_back(glm::vec2(uvwidth, uvheight));
+	uvs.push_back(glm::vec2(0.0f, uvheight));
+	
 	//GLuint _vertexbuffer;
 	glGenBuffers(1, &_vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, _vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
 /*
 	//GLuint _normalbuffer;
 	glGenBuffers(1, &_normalbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, _normalbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_normal_buffer_data), g_normal_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
 */
 	//GLuint _uvbuffer;
 	glGenBuffers(1, &_uvbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, _uvbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
 }
 
 void Mesh::generateLineMesh(Line* line)
