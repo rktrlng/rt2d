f50fb2094e7c2288231a078ebbb0d20e96561eae
rktrlng <rik.teerling@gmail.com>
2016-01-26 22:43:18 +0100

    tabs

--- rt2d/stringutil.h
+++ rt2d/stringutil.h
@@ -8,13 +8,13 @@
 // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52015
 namespace rt2d
 {
-  template<typename T>
-  inline std::string to_string(T val)
-  {
-    std::stringstream temp_str;
-    temp_str << (val);
-    return temp_str.str();
-  }
+    template<typename T>
+    inline std::string to_string(T val)
+    {
+        std::stringstream temp_str;
+        temp_str << (val);
+        return temp_str.str();
+    }
 }
 
 #endif
