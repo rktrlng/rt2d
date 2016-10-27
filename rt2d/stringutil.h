fa946791e8ae33a43494a4b05094673e4350ea1f
rktrlng <rik.teerling@gmail.com>
2016-10-27 14:32:08 +0200

    doxygen stringutil

--- rt2d/stringutil.h
+++ rt2d/stringutil.h
@@ -1,13 +1,34 @@
+/**
+ * @file stringutil.h
+ *
+ * @brief The stringutil header file.
+ *
+ * This file is part of RT2D, a 2D OpenGL framework.
+ *
+ * - Copyright 2015 Rik Teerling <rik@onandoffables.com>
+ *   - Initial commit
+ *
+ * Work around MinGW bug. Thanks Viktor [VZout].
+ * https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52015
+ */
+
 #ifndef STRINGUTIL_H_
 #define STRINGUTIL_H_
 
 #include <string>
 #include <sstream>
 
-// Work around MinGW bug. Thanks Viktor.
-// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=52015
 namespace rt2d
 {
+    /**
+	 * @brief converts any value to a string
+	 *
+	 * @param val
+	 * the value that needs to be converted to a string
+     *
+     * @return temp_str
+	 * the returned string
+	 */
     template<typename T>
     inline std::string to_string(T val)
     {
