ee282abb0f40f5be62644c78b3a16c0e039b3261
rktrlng <rik.teerling@gmail.com>
2016-12-16 14:42:43 +0100

    cleanup

--- rt2d/renderer.cpp
+++ rt2d/renderer.cpp
@@ -107,13 +107,12 @@ int Renderer::init()
 
 void Renderer::renderScene(Scene* scene)
 {
-
+	// Since apple scales automatically, this will cause bugs where the viewport gets very small.
 	#ifndef __APPLE__
-	// Since apple does this automatically. This will cause some bugs where the viewport gets very small.
-
-	// Set the glViewport to the width and height of the window.
-	glViewport(0, 0, scene->input()->getWindowWidth(), scene->input()->getWindowHeight());
+		// Set the glViewport to the width and height of the window.
+		glViewport(0, 0, scene->input()->getWindowWidth(), scene->input()->getWindowHeight());
 	#endif
+
 	// Clear the screen
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
